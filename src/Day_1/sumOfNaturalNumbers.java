e551426d3ab79f6b266d179d5900e32b799b6df7
Anirudh Srivastava <anirudhsrivastava225@gmail.com>
2026-05-28 23:35:22 +0530

    Initial commit with Day 1 Java files

---
+++ src/Day_1/sumOfNaturalNumbers.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class sumOfNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number of natural numbers");
+        long n = input.nextLong();
+        long sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
