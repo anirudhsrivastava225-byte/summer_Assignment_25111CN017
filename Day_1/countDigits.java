daf96695e6216fc818a482eb3215bb95cf014689
anirudhsrivastava225-byte <anirudhsrivastava225@gmail.com>
2026-05-29 21:34:52 +0530

    Update countDigits.java

--- Day_1/countDigits.java
+++ Day_1/countDigits.java
@@ -1,4 +1,16 @@
-package Day_1;
+import java.util.*;
 
 public class countDigits {
+  public static void main (String[] args){
+    Scanner input = new Scanner( System.in ) ;
+    System.out.println(" Enter a number : ") ;
+    int n = input.nextInt() ;
+    int count = 0 ;
+    int num = n ;
+    for (int i = 0 ; n > 0 ; i++ ) {
+      n = n / 10;
+      count ++ ;
+    }
+    System.out.println(" The total numbers in the digit " + num + " are " + count);
+  }
 }
