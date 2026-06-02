b6dc726e89ea48a6b4097660a17e6892fada6b87
Anirudh Srivastava <anirudhsrivastava225@gmail.com>
2026-06-02 21:23:04 +0530

    This is the 3rd commit of Day 2

---
+++ Day_2/reverseOfNumber.java
@@ -0,0 +1,18 @@
+package Day_2;
+import java.util.* ;
+public class reverseOfNumber {
+    public static void main( String[] args){
+     Scanner input = new Scanner(System.in);
+     System.out.println( " Enter a number: " );
+     int n = input.nextInt() ;
+     int num = n ;
+     int newN = 0 ;
+     for ( int i = 0 ; n > 0 ; i++ ){
+        int rem = n % 10 ;
+        newN = ( newN + rem ) * 10 ;
+        n = n / 10 ;
+     }
+     System.out.println ( " The reverse of " + num + " is " + newN/10);
+     
+   }
+}
