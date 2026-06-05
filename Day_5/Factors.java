f059713ad22be976c9ea787c59cecc7da5cfd7ea
Anirudh Srivastava <anirudhsrivastava225@gmail.com>
2026-06-05 23:08:30 +0530

    This is the 1st commit of Day 5

---
+++ Day_5/Factors.java
@@ -0,0 +1,36 @@
+package Day_5;
+import java.util.List ;
+import java.util.ArrayList ;
+import java.util.Scanner ;
+public class Factors {
+    private final int number ;
+     public Factors ( int n ) {
+        this.number = n ;
+    }
+    public List <Integer> extractFactor () {
+        final List < Integer > inventory = new ArrayList <> ( ) ;
+        for ( int i = 1 ; i <= Math.sqrt( number ) ; i++ ) {
+            if ( number % i == 0 ) inventory.add(i) ;
+        }
+        inventory.add( number ) ;
+        return inventory ;
+    }
+}
+class FactorsRunner {
+    public static void main(String[] args) {
+        Scanner input = new Scanner ( System.in ) ;
+        int j = 0 ;
+        System.out.println( "Enter the number to calculate the factors: " ) ;
+        int n = input.nextInt() ;
+        Factors test = new Factors ( n ) ;
+        List < Integer > storage = test.extractFactor() ;
+        System.out.println( "The factors of " + n + " are: ");
+        for ( Integer num : storage ) {
+            System.out.print( num );
+            j ++ ;
+            if ( j != storage.size()) System.out.print(",");
+            if ( j == storage.size()) System.out.print(".");
+        }
+        input.close() ;
+    }
+}
